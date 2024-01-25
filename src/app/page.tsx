eb8b1239ade029c6b4f52ecc20727f573111fa28
Joe Sanchez Jr <joe@joesanchezjr.com>
2024-01-24 20:19:14 -0600

    added dark mode support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { ModeToggle } from "@/components/mode-toggle"
+
 export default function Home() {
   return (
     <main className="min-h-screen-dynamic flex items-center justify-center">
-      <span>Hello World 👋🏽</span>
+      <ModeToggle />
     </main>
   )
 }
