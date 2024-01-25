e9eddfdb75d8135fcdb5c675cf84d7f7ec580dc4
Joe Sanchez Jr <joe@joesanchezjr.com>
2024-01-25 15:50:41 -0600

    Manually Set Production URL (#2)
    * manually set site url
    
    * checking env

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,11 +1,11 @@
 export function getUrl() {
-  let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    "http://localhost:3000/"
+  let url = process.env.NODE_ENV === "production"
+    ? "https://with-dash.vercel.app/"
+    : process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      "http://localhost:3000/";
   // Make sure to include `https://` when not localhost.
-  url = url.includes("http") ? url : `https://${url}`
+  url = url.includes("http") ? url : `https://${url}`;
   // Make sure to include a trailing `/`.
-  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`
-  return url
+  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+  return url;
 }
