a41eb2436bc71b405b38b45cb42c0035caa8489d
Joe Sanchez Jr <joe@joesanchezjr.com>
2024-01-25 15:44:22 -0600

    manually set site url

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,6 +1,6 @@
 export function getUrl() {
   let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    "https://with-dash.vercel.app" ?? // Set this to your site URL in production env.
     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
     "http://localhost:3000/"
   // Make sure to include `https://` when not localhost.
