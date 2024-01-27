4fedfb6cad5656071ca0c486cfa4bbc633641c1a
Joe Sanchez Jr <joe@joesanchezjr.com>
2024-01-26 20:05:23 -0600

    updated getUrl

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -2,7 +2,7 @@ export function getUrl() {
   let url =
     process.env.NODE_ENV === "production"
       ? "https://with-dash.vercel.app/"
-      : process?.env?.NEXT_PUBLIC_VERCEL_URL ?? "http://localhost:3000/" // Automatically set by Vercel.
+      : process?.env?.NEXT_PUBLIC_VERCEL_BRANCH_URL ?? "http://localhost:3000/" // Automatically set by Vercel.
   // Make sure to include `https://` when not localhost.
   url = url.includes("http") ? url : `https://${url}`
   // Make sure to include a trailing `/`.
